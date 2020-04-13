9e6a91e625cddfb93b0db8aa1f428ca97a0fa789
Yuan Wang <163165+yuanw@users.noreply.github.com>
2020-04-12 21:48:14 -0600

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
+let
+  bootstrap = import <nixpkgs> {};
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src {};
+  myHaskellPackages = pkgs.haskell.packages."${compiler}";
+
+  # myPackages = myHaskellPackages.callCabal2nix "project" ./blog.cabal {};
+in
+myHaskellPackages.shellFor {
+  withHoogle = true;
+  packages = p: [];
+  inherit ((import ./pre-commit.nix).pre-commit-check) shellHook;
+  buildInputs = with myHaskellPackages;
+    [
+      hlint
+      ghcid
+      cabal2nix
+      ormolu
+      cabal-install
+      cabal-fmt
+    ];
+}
